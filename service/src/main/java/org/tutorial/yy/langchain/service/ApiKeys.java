d8a9e4a9f4755c88cfddf74332b6e804ca46ed36
yyhuangfu <hfyy0726@outlook.com>
2024-01-29 00:51:52 +0800

    [feat] hello world!

--- service/src/main/java/org/tutorial/yy/langchain/service/ApiKeys.java
+++ service/src/main/java/org/tutorial/yy/langchain/service/ApiKeys.java
@@ -1,6 +1,15 @@
 package org.tutorial.yy.langchain.service;
 
 public class ApiKeys {
-    public static final String OPENAI_API_KEY = System.getenv("OPENAI_API_KEY");
+
+    // You can use "demo" api key for demonstration purposes.
+    // You can get your own OpenAI API key here: https://platform.openai.com/account/api-keys
+    public static final String OPENAI_API_KEY = "demo"; // System.getenv("OPENAI_API_KEY");
+
+    // You can get your own HuggingFace API key here: https://huggingface.co/settings/tokens
+    public static final String GPT_API_KEY = System.getenv("HF_API_KEY");
+
+    // You can get your own Judge0 RapidAPI key here: https://rapidapi.com/judge0-official/api/judge0-ce
+    public static final String RAPID_API_KEY = System.getenv("RAPID_API_KEY");
 
 }
