5313e85e00699f9293e81d82fcdef0fd13e2dbfa
yyhuangfu <hfyy0726@outlook.com>
2024-01-29 00:51:52 +0800

    [feat] hello world!

--- demo/src/main/java/org/tutorial/yy/langchain/demo/HelloLangChain.java
+++ demo/src/main/java/org/tutorial/yy/langchain/demo/HelloLangChain.java
@@ -1,23 +1,25 @@
 package org.tutorial.yy.langchain.demo;
 
-import dev.langchain4j.data.message.AiMessage;
-import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import org.tutorial.yy.langchain.service.ApiKeys;
+
+
+/**
+ * @author yyHuangfu
+ * @create 2024/1/28
+ */
 
 public class HelloLangChain {
 
     public static void main(String[] args) {
 
         // Create an instance of a model
-        OpenAiChatModel model = OpenAiChatModel
-                .withApiKey(ApiKeys.OPENAI_API_KEY);
+        ChatLanguageModel model = OpenAiChatModel.withApiKey(ApiKeys.OPENAI_API_KEY);
 
         // Start interacting
-        AiMessage answer = model.generate(
-                UserMessage.from("Hello world!")
-        ).content();
-        // Hello! How can I assist you today?
-        System.out.println(answer.text());
+        String answer = model.generate("你好我是皇甫一宇，你能做个自我介绍吗？");
+
+        System.out.println(answer);
     }
 }
